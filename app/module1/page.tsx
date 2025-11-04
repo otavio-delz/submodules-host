3f1a7d8ef0288a6ef9fe4d1192273e5fca5052ad
Otavio Zirpoli <otavio.zirpoli@compasso.com.br>
2025-11-04 12:13:13 -0300

    add module page

---
+++ app/module1/page.tsx
@@ -0,0 +1,3 @@
+import Module1Page from "@/modules/submodules-module1/pages/module1";
+
+export default Module1Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Module1Component } from "@/modules/submodules-module1/module1-component";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Module1Component />
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black flex-col gap-4">
+      Home
+      <Link className="text-blue-500 underline" href="/module1">
+        Module 1
+      </Link>
     </div>
   );
 }
